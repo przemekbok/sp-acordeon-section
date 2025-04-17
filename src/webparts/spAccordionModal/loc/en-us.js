3479a92a6cb1ac042f0df891326a5aa772f787b9
Przemek <34289616+przemekbok@users.noreply.github.com>
2025-04-17 13:45:14 +0200

    Update en-us strings for accordion webpart

--- src/webparts/spAccordionModal/loc/en-us.js
+++ src/webparts/spAccordionModal/loc/en-us.js
@@ -1,8 +1,11 @@
 define([], function() {
   return {
-    "PropertyPaneDescription": "Description",
-    "BasicGroupName": "Group Name",
-    "DescriptionFieldLabel": "Description Field",
+    "PropertyPaneDescription": "Configure your accordion section",
+    "BasicGroupName": "General Settings",
+    "DescriptionFieldLabel": "Webpart Title",
+    "ListNameFieldLabel": "List Name",
+    "ListConfigGroupName": "List Configuration",
+    "ListNameFieldDescription": "Enter the name of the SharePoint list containing accordion items",
     "AppLocalEnvironmentSharePoint": "The app is running on your local environment as SharePoint web part",
     "AppLocalEnvironmentTeams": "The app is running on your local environment as Microsoft Teams app",
     "AppLocalEnvironmentOffice": "The app is running on your local environment in office.com",
@@ -11,6 +14,9 @@ define([], function() {
     "AppTeamsTabEnvironment": "The app is running in Microsoft Teams",
     "AppOfficeEnvironment": "The app is running in office.com",
     "AppOutlookEnvironment": "The app is running in Outlook",
-    "UnknownEnvironment": "The app is running in an unknown environment"
+    "UnknownEnvironment": "The app is running in an unknown environment",
+    "LoadingMessage": "Loading accordion items...",
+    "NoItemsMessage": "No items found. Please add items to the list or configure the webpart properties.",
+    "ConfigureListMessage": "Please configure a list name in the webpart properties."
   }
 });
